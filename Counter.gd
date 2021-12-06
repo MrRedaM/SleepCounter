5e59ce80856ca4d7941245fd8fa4f0865101695d
MrRedaM <r.morsli@esi-sba.dz>
2021-12-06 21:35:48 +0100

    Fix glow

--- Counter.gd
+++ Counter.gd
@@ -7,15 +7,16 @@ var date = OS.get_unix_time_from_datetime (
 		"day":4} )
 
 onready var _counter_label := $VBoxContainer/HBoxContainer/DaysCount
+onready var _animation := $AnimationPlayer
 
 func _ready():
 	update_counter()
+	_animation.play("glow")
 
 
 func update_counter():
 		var current_date = OS.get_unix_time_from_datetime(OS.get_datetime())
 		_counter_label.text = str((current_date - date) / 60 / 60 / 24)
 
-
-func _on_RefreshTimer_timeout():
+func _on_AnimationPlayer_animation_finished(anim_name):
 	update_counter()
