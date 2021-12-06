18a2a907171e74a113ac9052699b682a1d6b6fe1
MrRedaM <r.morsli@esi-sba.dz>
2021-12-06 19:48:59 +0100

    Initial Commit

---
+++ Counter.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var date = OS.get_unix_time_from_datetime (
+	 { 
+		"year": 2021, 
+		"month": 12, 
+		"day":4} )
+
+onready var _counter_label := $VBoxContainer/HBoxContainer/DaysCount
+
+func _ready():
+	update_counter()
+
+
+func update_counter():
+		var current_date = OS.get_unix_time_from_datetime(OS.get_datetime())
+		_counter_label.text = str((current_date - date) / 60 / 60 / 24)
+
+
+func _on_RefreshTimer_timeout():
+	update_counter()
